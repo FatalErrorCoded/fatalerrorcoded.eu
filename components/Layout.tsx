47cf618457e2667a2cd90e3f1de38d2ab058cc0d
Martin Löffler <git@fatalerrorcoded.eu>
2020-01-03 17:18:24 +0100

    Back where we were

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,42 +1,32 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import * as React from 'react';
+import Head from 'next/head';
+
+import "./Layout.module.scss";
 
 type Props = {
-  title?: string
+    title?: string
 }
 
 const Layout: React.FunctionComponent<Props> = ({
-  children,
-  title = 'This is the default title',
+    children,
+    title = 'fatalerrorcoded.eu',
 }) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+    <div className="main-wrapper">
+        <div className="main-content">
+            <Head>
+                <title>{title}</title>
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <header className="main-header">
+                <h2><code>fatalerrorcoded.eu</code></h2>
+            </header>
+            <main>
+                {children}
+            </main>
+        </div>
+    </div>
+);
 
-export default Layout
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react'
-import Link from 'next/link'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
-  return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <section>
+                <h1>Who the frick are you?</h1>
+                <p>Hello there. I am a 16 year old Czech dude who does coding and stuff.</p>
+            </section>
+            <section>
+                <h1>What languages and IDE do you use?</h1>
+                <p>My favorite IDE is Visual Studio Code, running on top of glorious Manjaro Linux</p>
+                <p>I am a jack of all trades as well, most experienced with Javascript, C++ and Rust, and some experience with Java, C# and PHP (HTML included as well, obviously)</p>
+            </section>
+        </Layout>
+    )
 }
 
 export default IndexPage
