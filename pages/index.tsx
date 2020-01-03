3d6e89b893231500d067d2bbdc9c4bacbc84cf25
Martin Löffler <git@fatalerrorcoded.eu>
2020-01-03 17:56:41 +0100

    Some projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,45 @@ import * as React from 'react'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
 
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+type ProjectProps = {
+    name: string,
+    repository?: string
+    status?: string
+}
+
+const Project: React.FunctionComponent<ProjectProps> = (props) => {
+    return (
+        <ExpansionPanel>
+            <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+            >
+                <b>{props.name}</b>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+                <div>
+                    {props.children}
+                    <br /><br />
+
+                    {props.status &&
+                        <>
+                            <span style={{ color: "#909090" }}>Status: {props.status}</span>
+                            <br />
+                        </>
+                    }
+                    {props.repository && <a target="_blank" href={props.repository}>
+                        Repository
+                    </a>}
+                </div>
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+    );
+}
+
 const IndexPage: NextPage = () => {
     return (
         <Layout>
@@ -13,6 +52,20 @@ const IndexPage: NextPage = () => {
                 <h1>What languages and IDE do you use?</h1>
                 <p>My favorite IDE is Visual Studio Code, running on top of glorious Manjaro Linux</p>
                 <p>I am a jack of all trades as well, most experienced with Javascript, C++ and Rust, and some experience with Java, C# and PHP (HTML included as well, obviously)</p>
+            </section>
+            <section>
+                <h1>My projects</h1>
+                <Project
+                    name="guiworks" repository="https://github.com/fatalerrorcoded/guiworks" status="Active"
+                >
+                    <span>A React-style Discord.js library for interactive embeds</span>
+                </Project>
+                <Project
+                    name="memeos" repository="https://gitlab.insrt.uk/fatalerrorcoded/memeos" status="Active"
+                >
+                    <span style={{ textDecoration: "line-through" }}>An epic meme OS for epic memers</span><br />
+                    <span>An attempt at an operating system in Rust</span>
+                </Project>
             </section>
         </Layout>
     )
