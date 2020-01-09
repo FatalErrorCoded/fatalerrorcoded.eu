4d8982294cf0fd2f7ed278fe45d9fc32ba301528
Martin Loffler <git@fatalerrorcoded.eu>
2020-01-09 06:15:22 +0100

    Blog post list

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,13 +1,42 @@
 import { NextPage } from "next";
+import Link from "next/link";
+import axios from "axios";
 
 import Layout from "../../components/Layout";
 
-const BlogIndexPage: NextPage = () => {
+const BlogIndexPage: NextPage<{posts?: any, status: number}> = ({ posts, status }) => {
+    if (status !== 200) {
+        return (
+            <Layout isBlog={true} canonical={"/blog"}>
+                <span>{status}</span>
+            </Layout>
+        )
+    }
+
+    let post_tags = [];
+    for (let post of posts.posts) {
+        post_tags.push(
+            <div key={post.id}>
+                <h2 style={{margin: "0px"}}><Link href={`/blog/${post.id}`}><a>{post.title}</a></Link></h2>
+                <p style={{color: "#909090"}}>{post.summary}</p>
+            </div>
+        )
+    }
+
     return (
-        <Layout isBlog={true} canonical="/blog">
-            <span>BLOG POSTS GO HERE</span>
+        <Layout isBlog={true} canonical={"/blog"}>
+            {post_tags}
         </Layout>
     );
+}
+
+BlogIndexPage.getInitialProps = async () => {
+    let res = await axios.get(`http://localhost:3000/api/posts`);
+
+    return {
+        posts: res.status === 200 ? res.data.data : undefined,
+        status: res.status
+    }
 }
 
 export default BlogIndexPage;
